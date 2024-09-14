be5ded9fe719276c0a50724d7ca0c93e9755ec95
Dhughes666 <graziemolto6@gmail.com>
2024-09-14 05:56:29 +0100

    Admin ...on point

--- polls2/admin.py
+++ polls2/admin.py
@@ -3,5 +3,26 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # Method I
+    # fields = ["pub_date", "question_text"]
+
+    # Method II
+    """
+    This is for multiple form fields. The first element of each tuple in
+    the fieldset is the title of the fieldset
+    """
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
+
--- polls2/models.py
+++ polls2/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateTimeField("publication_date")
 
     def __str__(self):
         return self.question_text
